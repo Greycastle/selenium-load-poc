83abdd7ac8763c530a81e0b02096687977d74e03
David <ddikman@gmail.com>
2019-06-27 10:34:28 +0100

    Removed generator projects and cleaned up code for test

--- readme.md
+++ readme.md
@@ -1,12 +1,28 @@
 # Selenium Load PoC
 
-And this is how you run it 🎶
-```bash
+This is an example of how to build a simple Selenium test and execute it from inside a docker instance.
+
+
+# Build and run docker
+It will run against a simple website, bundled within this package. So before running the test, start the web server:
+```shell
+dotnet run -p src/SeleniumLoadPoc.TestApp
+```
+
+Then build the service:
+```shell
 docker build .
+```
+
+Then running the docker instance will run the test:
+```shell
 docker run -e TARGET_HOSTNAME=172.16.211.170 40df9915370b
 ```
 
-And this is how you load it
-```bash
-dotnet run -p src/SeleniumLoadPoc.Generator --clients 5 --docker-name 40df9915370b
+# Run locally
+To troubleshoot you can also build and run locally:
+
+```
+dotnet run -p src/SeleniumLoadPoc.TestApp
+dotnet run -p src/SeleniumLoadPoc
 ```
--- src/SeleniumLoadPoc/Program.cs
+++ src/SeleniumLoadPoc/Program.cs
@@ -9,42 +9,103 @@ namespace SeleniumLoadPoc
 {
     public class Program
     {
-        public static void Main()
+        public static int Main()
         {
             var stopwatch = Stopwatch.StartNew();
-            var generator = new Generator();
+            try
+            {
+                RunTest();
+                Console.WriteLine($"Completed in {stopwatch.ElapsedMilliseconds}ms");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Test run failed!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
 
-            var hostname = Environment.GetEnvironmentVariable("TARGET_HOSTNAME") ?? "localhost";
+            return 0;
+        }
 
-            var codename = generator.Generate();
+        private static ChromeOptions GetDriverOptions()
+        {
             var options = new ChromeOptions();
             options.AddArgument("headless");
             options.AddArgument("window-size=800,600");
             options.AddArgument("disable-gpu");
             options.AddArgument("no-sandbox");
             options.AddArgument("ignore-certificate-errors");
-
             options.AcceptInsecureCertificates = true;
-            var driver = new ChromeDriver(options);
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            return options;
+        }
 
-            driver.Navigate().GoToUrl($"https://{hostname}:5001");
-            const string expectedTitle = "Home Page - SeleniumLoadPoc.TestApp";
-            if (driver.Title != expectedTitle)
+        private static void RunTest()
+        {
+            var generator = new Generator();
+            var hostname = Environment.GetEnvironmentVariable("TARGET_HOSTNAME") ?? "localhost";
+            var codename = generator.Generate();
+
+            using (IWebDriver driver = new ChromeDriver(GetDriverOptions()))
             {
-                throw new Exception($"Expected title '{driver.Title}' to be '{expectedTitle}'");
+                var testSteps = new TestSteps(driver, $"https://{hostname}:5001");
+                testSteps.OpenWebSite();
+
+                testSteps.SetInput(codename);
+                testSteps.Submit();
+                testSteps.CheckThatOutputIs(codename);
+
+                driver.Close();
             }
-            wait.Until(webdriver => webdriver.FindElement(By.Id("input"))).SendKeys(codename);
-            driver.FindElement(By.Id("submit")).Click();
-            var outputElement = wait.Until(webDriver => webDriver.FindElement(By.Id("output")));
-            if (codename != outputElement.Text)
+        }
+    }
+
+    public class TestSteps
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+        private readonly string _rootUrl;
+
+        public TestSteps(IWebDriver driver, string rootUrl)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            _rootUrl = rootUrl;
+        }
+
+        private IWebElement FindElementById(string id)
+        {
+            return _wait.Until(webdriver => webdriver.FindElement(By.Id(id)));
+        }
+
+        public void OpenWebSite()
+        {
+            _driver.Navigate().GoToUrl(_rootUrl);
+            ShouldBeEqual("Home Page - SeleniumLoadPoc.TestApp", _driver.Title, "title");
+        }
+
+        private static void ShouldBeEqual(string expected, string actual, string propertyName)
+        {
+            if (actual != expected)
             {
-                throw new Exception($"Expected output text '{outputElement.Text}' to be '{codename}'");
+                throw new Exception($"Expected {propertyName} '{actual}' to be '{expected}'");
             }
+        }
 
-            driver.Close();
+        public void SetInput(string value)
+        {
+            FindElementById("input").SendKeys(value);
+        }
 
-            Console.WriteLine($"Completed in {stopwatch.ElapsedMilliseconds}ms");
+        public void Submit()
+        {
+            FindElementById("submit").Click();
+        }
+
+        public void CheckThatOutputIs(string value)
+        {
+            var output = FindElementById("output");
+            ShouldBeEqual(value, output.Text, "output text");
         }
     }
 }
